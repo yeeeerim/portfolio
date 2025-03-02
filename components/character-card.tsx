8ff0d71c6184b623a2ac6564c17d9f0c433a9dd8
yerim <pppp5097@naver.com>
2025-03-02 16:59:01 +0900

    design: 캐릭터 카드 디자인 변경

--- components/character-card.tsx
+++ components/character-card.tsx
@@ -1,14 +1,16 @@
 "use client";
 
-import { Button } from "@heroui/button";
 import { Card, CardFooter, Image } from "@heroui/react";
+import clsx from "clsx";
 import React from "react";
 
 interface CharacterCardProps {
-  src: string;
+  src?: string;
   title?: string;
   description?: string;
   buttonText?: string;
+  active?: boolean;
+  onClick?: () => void;
 }
 
 const CharacterCard = ({
@@ -16,31 +18,53 @@ const CharacterCard = ({
   title,
   description,
   buttonText,
+  active,
+  onClick,
 }: CharacterCardProps) => {
+  if (!src)
+    return <Card className="border-none w-[100px] h-[100px]" radius="lg" />;
+
   return (
-    <div>
-      <Card isFooterBlurred className="border-none" radius="lg">
+    <div onClick={onClick}>
+      <Card
+        isFooterBlurred
+        className={clsx("text-white/80 w-[100px] h-[100px] relative", {
+          "border-2 p-1 border-white/50 rounded-2xl": active,
+          "border-none": !active,
+        })}
+        radius="lg"
+      >
         <Image
           alt="Character Image"
-          className="object-cover"
-          height={200}
+          className="object-cover aspect-square"
           src={src}
-          width={200}
         />
-        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-          <ul>
-            <li className="text-sm text-white/80 font-semibold">{title}</li>
-            <li className="text-tiny text-white/80">{description}</li>
-          </ul>
-          <Button
-            className="text-sm font-semibold text-white bg-black/20"
-            color="default"
-            radius="lg"
-            size="sm"
-            variant="flat"
-          >
-            {buttonText}
-          </Button>
+        <button
+          className={clsx(
+            "rounded-2xl absolute font-semibold bg-black/20 z-10 px-2 py-1 text-[10px]",
+            {
+              "right-2 top-2": active,
+              "right-1 top-1": !active,
+            }
+          )}
+        >
+          {buttonText}
+        </button>
+        <CardFooter
+          className={clsx(
+            "justify-start before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large shadow-small ml-1 z-10",
+            {
+              "w-[calc(100%_-_16px)] bottom-2": active,
+              "w-[calc(100%_-_8px)] bottom-1": !active,
+            }
+          )}
+        >
+          <p className="flex flex-col">
+            <span className="text-tiny flex items-center font-semibold">
+              {title}
+            </span>
+            <span className="text-tiny">{description}</span>
+          </p>
         </CardFooter>
       </Card>
     </div>
--- components/character-list.tsx
+++ components/character-list.tsx
@@ -3,15 +3,31 @@
 import React from "react";
 import { Button } from "@heroui/button";
 
-import Profile from "@/components/profile";
+import CharacterCard from "./character-card";
 
 const CharacterList = () => {
+  const [selectedCharacter, setSelectedCharacter] = React.useState("진예림");
+
   return (
     <div className="flex flex-col justify-between">
       <div className="flex w-[120px] items-center flex-col gap-4">
-        <Profile />
-        <Profile />
-        <Profile />
+        <CharacterCard
+          active={selectedCharacter === "진예림"}
+          buttonText="Lv. 3"
+          description="Frontend"
+          src={"https://heroui.com/images/hero-card.jpeg"}
+          title="진예림"
+          onClick={() => setSelectedCharacter("진예림")}
+        />
+        <CharacterCard
+          active={selectedCharacter === "OOO"}
+          buttonText="Lv. 1"
+          description="Frontend"
+          src={"https://heroui.com/images/album-cover.png"}
+          title="OOO"
+          onClick={() => setSelectedCharacter("OOO")}
+        />
+        <CharacterCard />
       </div>
       <Button color="success" onPress={() => {}}>
         START
