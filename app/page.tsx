cb1ceddc113b23cb7bf7ab459abe2f5f40dd0d86
yerim <pppp5097@naver.com>
2025-03-02 17:49:46 +0900

    feat: 캐릭터 정보 페이지 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
+import CharacterInfo from "@/components/character-info";
 import CharacterList from "@/components/character-list";
 
 export default function Home() {
   return (
     <section className="flex justify-between h-full gap-10 py-8 md:py-10">
       <CharacterList />
-      <div className="flex-1 h-full border" />
-      <div className="flex-1 h-full border" />
+      <div className="flex-1 h-full border border-default-100" />
+      <CharacterInfo />
     </section>
   );
 }
---
+++ components/character-info.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { Progress } from "@heroui/react";
+import React from "react";
+
+const CharacterInfo = () => {
+  return (
+    <section className="flex-1 h-full bg-default-100/50 p-7 text-default-600 flex flex-col gap-2">
+      <span className="font-black text-lg">LV. 3</span>
+      <span>진예림 (JIN YERIM) / Frontend Engineer</span>
+      <ul className="flex gap-2 uppercase text-tiny">
+        <li className="rounded-sm text-default-400 px-1 font-semibold bg-default-200/90">
+          React
+        </li>
+        <li className="rounded-sm text-default-400 px-1 font-semibold bg-default-200/90">
+          Next.js
+        </li>
+        <li className="rounded-sm text-default-400 px-1 font-semibold bg-default-200/90">
+          Typescript
+        </li>
+        <li className="rounded-sm text-default-400 px-1 font-semibold bg-default-200/90">
+          Tailwind
+        </li>
+      </ul>
+      <div className="mt-4">
+        <h4 className="border-b border-white/20">Skill</h4>
+        <div className="flex flex-col gap-2 w-full">
+          <Progress
+            classNames={{
+              track: "drop-shadow-md border border-default",
+              indicator: "bg-gradient-to-r from-cyan-500 to-green-500",
+              label: "tracking-wider font-medium text-default-600",
+              value: "text-foreground/60",
+            }}
+            color="warning"
+            label="React"
+            maxValue={100}
+            showValueLabel={true}
+            size="sm"
+            value={75}
+          />
+          <Progress
+            classNames={{
+              track: "drop-shadow-md border border-default",
+              indicator: "bg-gradient-to-r from-purple-500 to-pink-500",
+              label: "tracking-wider font-medium text-default-600",
+              value: "text-foreground/60",
+            }}
+            color="warning"
+            label="Next.js"
+            maxValue={100}
+            showValueLabel={true}
+            size="sm"
+            value={60}
+          />
+          <Progress
+            classNames={{
+              track: "drop-shadow-md border border-default",
+              indicator: "bg-gradient-to-r from-green-500 to-yellow-500",
+              label: "tracking-wider font-medium text-default-600",
+              value: "text-foreground/60",
+            }}
+            color="warning"
+            label="Vue.js"
+            maxValue={100}
+            showValueLabel={true}
+            size="sm"
+            value={25}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default CharacterInfo;
