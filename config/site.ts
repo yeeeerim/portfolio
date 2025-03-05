706b7ce925d1e4db830091d78d0251fce6712f85
yerim <pppp5097@naver.com>
2025-03-05 11:25:12 +0900

    remove: 사용하지 않는 파일 제거

--- config/site.ts
+++ config/site.ts
@@ -4,60 +4,12 @@ export const siteConfig = {
   name: "진예림 | 프론트엔드 개발자 포트폴리오",
   description: "프론트엔드 개발자 진예림의 포트폴리오입니다.",
   navItems: [
-    // {
-    //   label: "Home",
-    //   href: "/",
-    // },
-    // {
-    //   label: "Docs",
-    //   href: "/docs",
-    // },
-    // {
-    //   label: "Pricing",
-    //   href: "/pricing",
-    // },
-    // {
-    //   label: "Blog",
-    //   href: "/blog",
-    // },
-    // {
-    //   label: "About",
-    //   href: "/about",
-    // },
     {
       label: "STORE",
       href: "/store",
     },
   ],
   navMenuItems: [
-    {
-      label: "Profile",
-      href: "/profile",
-    },
-    {
-      label: "Dashboard",
-      href: "/dashboard",
-    },
-    {
-      label: "Projects",
-      href: "/projects",
-    },
-    {
-      label: "Team",
-      href: "/team",
-    },
-    {
-      label: "Calendar",
-      href: "/calendar",
-    },
-    {
-      label: "Settings",
-      href: "/settings",
-    },
-    {
-      label: "Help & Feedback",
-      href: "/help-feedback",
-    },
     {
       label: "Logout",
       href: "/logout",
