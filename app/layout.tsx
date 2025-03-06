82c22c40d83c89dc746c8a35394e9ca271e300d6
yerim <pppp5097@naver.com>
2025-03-06 14:03:54 +0900

    design: 폰트 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import clsx from "clsx";
 import { Providers } from "./providers";
 
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
+import { fontCinzel } from "@/config/fonts";
 
 export const metadata: Metadata = {
   title: {
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html suppressHydrationWarning lang="en">
       <head />
-      <body className={clsx("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
+      <body className={clsx("min-h-screen bg-background font-cinzel antialiased", fontCinzel.variable)}>
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="container relative h-screen mx-auto flex-1 max-w-7xl flex-grow">{children}</div>
         </Providers>
--- config/fonts.ts
+++ config/fonts.ts
@@ -1,11 +1,21 @@
-import { Fira_Code as FontMono, Inter as FontSans } from "next/font/google";
+import { Fira_Code as FontMono, Inter as FontSans, Cinzel, Noto_Serif } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
 
+export const fontSerif = Noto_Serif({
+  subsets: ["latin"],
+  variable: "--font-serif",
+});
+
 export const fontMono = FontMono({
   subsets: ["latin"],
   variable: "--font-mono",
+});
+
+export const fontCinzel = Cinzel({
+  subsets: ["latin"],
+  variable: "--font-cinzel",
 });
