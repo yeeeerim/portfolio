c7ba6e219a2f65723afdf12737ffc30066531825
yerim <pppp5097@naver.com>
2025-03-10 17:06:07 +0900

    feat: 무기 리스트 데이터 연동

--- app/(withLayout)/character/_components/weapons.tsx
+++ app/(withLayout)/character/_components/weapons.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
+import WeaponItem from "./weapon-item";
+
+import { weaponList } from "@/static/weapon-data";
+
 const Weapons = () => {
   return (
     <div className="flex h-fit flex-col">
       <h4 className="h-[30px]">Weapons</h4>
       <div className="grid gap-2 flex-1 h-[550px] max-h-[550px] overflow-y-auto grid-cols-3 items-center justify-center">
         {Array.from({ length: 30 }, (_, index) => (
-          <div
+          <WeaponItem
             key={index}
-            className="border w-[90px] aspect-square border-default-200"
+            item={weaponList.length > index ? weaponList[index] : null}
           />
         ))}
       </div>
