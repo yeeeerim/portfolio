81743619ffc67cf005e7a60d33c049fcb79ac2af
yerim <pppp5097@naver.com>
2025-03-10 17:06:38 +0900

    design: 캐릭터, 아이템 상세 UI 임시 구현

--- app/(withLayout)/character/_components/character-detail.tsx
+++ app/(withLayout)/character/_components/character-detail.tsx
@@ -1,12 +1,64 @@
+"use client";
+
+import { Progress } from "@heroui/react";
 import React from "react";
 
+import { calculateDeveloperLevel } from "@/lib/calculateDeveloperLevel";
+import { careerData } from "@/static/career-data";
+
 const CharacterDetail = () => {
+  // 결과 계산
+  const result = calculateDeveloperLevel(careerData);
+  const { level, experience } = result;
+
   return (
     <div className="h-[600px] w-[400px] px-5 bg-default-100/60 divide-y divide-default-200/60 flex-col flex items-start justify-center">
-      <div className="w-full h-[300px] text-center flex items-center justify-center">
-        {/* Character Info
-        <br />
-        (Level, Name, HP, MP, EXP) */}
+      <div className="w-full flex-col p-4 h-[300px] flex">
+        <h3 className="text-center font-bold">JIN YERIM</h3>
+        <span>Frontend</span>
+        <span className="text-3xl font-bold mb-3">{`Level ${level}`}</span>
+        <div className="flex items-center w-full gap-3">
+          <Progress
+            classNames={{
+              base: "flex-row items-center",
+              track: "flex-1 border border-default-300",
+              indicator: "bg-[#ffd86cee]",
+              label: "text-sm w-[30px]",
+            }}
+            label="EXP"
+            radius="none"
+            value={experience}
+          />
+          <span className="w-[50px]">{experience} %</span>
+        </div>
+        <div className="flex items-center w-full gap-3">
+          <Progress
+            classNames={{
+              base: "flex-row items-center",
+              track: "flex-1 border border-default-300",
+              indicator: "bg-[#ff765eee]",
+              label: "text-sm w-[30px]",
+            }}
+            label="HP"
+            radius="none"
+            value={100}
+          />
+          <span className="w-[50px]">100</span>
+        </div>
+        <div className="flex items-center w-full gap-3">
+          <Progress
+            classNames={{
+              base: "flex-row items-center",
+              track: "flex-1 border border-default-300",
+              indicator: "bg-[#63bcffee]",
+              label: "text-sm w-[30px]",
+            }}
+            label="MP"
+            radius="none"
+            value={100}
+          />
+          <span className="w-[50px]">100</span>
+        </div>
       </div>
       <div className="w-full text-center flex-1 flex items-center justify-center">
         {/* Character Feature 1 */}
--- app/(withLayout)/character/_components/item-detail.tsx
+++ app/(withLayout)/character/_components/item-detail.tsx
@@ -1,12 +1,73 @@
+"use client";
+
+import { Progress } from "@heroui/react";
 import React from "react";
+
+import { Mp5Icon } from "@/components/icons";
 
 const ItemDetail = () => {
   return (
-    <div className="w-[250px] flex h-fit pt-[40px]">
+    <div className="w-[280px] flex h-fit pt-[40px]">
+      {/* 삼각형 */}
       <div className="h-[90px] flex items-center ">
-        <div className="w-[20px] h-[30px] -rotate-90 bg-transparent border-t-[15px] border-l-[20px] border-r-[20px] border-transparent border-t-[#ffe38848]" />
+        <div className="w-[20px] h-[30px] -mx-2 -rotate-90 bg-transparent border-t-[15px] border-l-[20px] border-r-[20px] border-transparent border-t-[#ffe38848]" />
       </div>
-      <div className="min-h-[550px] h-[550px] bg-default-100/60 w-full" />
+      {/* 내용 */}
+      <div className="min-h-[550px] flex flex-col h-[550px] bg-default-100/60 w-full p-4">
+        <div className="flex items-center justify-between">
+          <span className="text-2xl font-semibold">React</span>
+          <span className="border rounded-full border-default-300 px-2 py-1 bg-default-100 text-tiny">
+            + Equipped
+          </span>
+        </div>
+        <span className="text-tiny mb-3">FrontEnd</span>
+        {/* 무기 아이콘 */}
+        <div className="flex gap-5 items-center">
+          <div className="border mb-3 flex items-center justify-center w-[90px] aspect-square border-default-200">
+            <Mp5Icon height={60} width={60} />
+          </div>
+          <span className="flex flex-col">
+            <span>Damage</span>
+            <span className="text-3xl mb-2 font-bold">+77</span>
+          </span>
+        </div>
+        {/* 속도 */}
+        <Progress
+          classNames={{
+            base: "flex-row items-center justify-between",
+            track: "w-[100px] border border-default-300",
+            label: "text-sm",
+          }}
+          color="default"
+          label="Speed"
+          radius="none"
+          value={80}
+        />
+        {/* 숙련도 */}
+        <Progress
+          classNames={{
+            base: "flex-row items-center justify-between",
+            track: "w-[100px] border border-default-300",
+            label: "text-sm",
+          }}
+          color="default"
+          label="Proficiency"
+          radius="none"
+          value={75}
+        />
+        {/* 지식 */}
+        <Progress
+          classNames={{
+            base: "flex-row items-center justify-between",
+            track: "w-[100px] border border-default-300",
+            label: "text-sm",
+          }}
+          color="default"
+          label="Knowledge"
+          radius="none"
+          value={75}
+        />
+      </div>
     </div>
   );
 };
