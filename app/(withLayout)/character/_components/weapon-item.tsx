7064dbb3125293d1170bfb485b82fe639768b982
yerim <pppp5097@naver.com>
2025-03-11 14:18:38 +0900

    remove: 불필요한 css 제거

--- app/(withLayout)/character/_components/weapon-item.tsx
+++ app/(withLayout)/character/_components/weapon-item.tsx
@@ -22,12 +22,7 @@ const WeaponItem = ({ item, onSelect, active = false }: WeaponItemProps) => {
       )}
       onClick={() => onSelect(item)}
     >
-      <span
-        className="absolute text-tiny font-semibold text-yellow-300 bottom-0 right-1"
-        style={{
-          textShadow: "0px 0px 2px #000000",
-        }}
-      >
+      <span className="absolute text-tiny font-semibold text-yellow-300 bottom-0 right-1">
         {item.name}
       </span>
       {item.icon}
