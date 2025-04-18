4d8fc28470831f2d5180fcbd25d66636d99f4c7a
yerim <pppp5097@naver.com>
2025-04-18 21:32:32 +0900

    refactor: 불필요한 use client 제거

--- app/(withLayout)/achievement/[slug]/_components/post-header.tsx
+++ app/(withLayout)/achievement/[slug]/_components/post-header.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
-import { getCharacterInfo } from "@/lib/getCharacterinfo";
-import { Character } from "@/types/character";
 import { CrossSwordsIcon } from "@/components/icons";
 
 interface PostHeaderProps {
@@ -12,22 +6,11 @@ interface PostHeaderProps {
   coverImage?: string;
 }
 
-const PostHeader = ({ title, date, coverImage }: PostHeaderProps) => {
-  const [character, setCharacter] = useState<Character>();
-
-  useEffect(() => {
-    const fetchedCharacter = getCharacterInfo();
-
-    setCharacter(fetchedCharacter);
-  }, []);
-
-  if (!character) {
-    return null;
-  }
-
+const PostHeader = ({ title, date }: PostHeaderProps) => {
   return (
     <div className="flex gap-1 flex-col">
-      <CrossSwordsIcon className="w-5 h-5" /> <h4>{title} 회고</h4>
+      <CrossSwordsIcon className="w-5 h-5" />{" "}
+      <h4 className="mt-2">{title} 회고</h4>
       <p className="text-sm text-default-500">{date}</p>
     </div>
   );
