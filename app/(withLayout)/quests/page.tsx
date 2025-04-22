c273bcab56df43602030809b993c3046c78a895d
yerim <pppp5097@naver.com>
2025-04-22 14:59:58 +0900

    feat: 퀘스트 미선택 UI 추가

--- app/(withLayout)/quests/page.tsx
+++ app/(withLayout)/quests/page.tsx
@@ -1,5 +1,11 @@
 const page = () => {
-  return <div className="min-h-[500px]" />;
+  return (
+    <div className="min-h-[500px] flex flex-col items-center justify-center">
+      <p className="text-2xl font-bold text-default-400">
+        퀘스트를 선택해주세요.
+      </p>
+    </div>
+  );
 };
 
 export default page;
